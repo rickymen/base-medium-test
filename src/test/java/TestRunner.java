3f374c9d9a814c94e36bcb92c0668a641856f5e7
Rickyfernando <13518062@std.stei.itb.ac.id>
2022-07-16 21:21:59 +0700

    done second

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -8,14 +8,16 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 
 public class TestRunner {
     private WebHomePage webHomePage;
     private WebLoginPage webLoginPage;
+    private WebMembershipPage webMembershipPage;
 
     private MobileHomePage mobileHomePage;
     private MobileLoginPage mobileLoginPage;
+    private MobileMembershipPage mobileMembershipPage;
 
     @DataProvider(name = "drivers", parallel = true)
     public Object[][] getDataProvider() {
@@ -67,22 +69,14 @@ public class TestRunner {
 
     @Test(dataProvider = "drivers")
     public void ut(String platform, RemoteWebDriver platformDriver) {
-        String username = getPropertyByPlatform("login.twitter.username", platform);
-        String password = getPropertyByPlatform("login.twitter.password", platform);
         String searchTitle = "sprint boot";
         String expectedTitle = "How to scale Microservices with Message Queues, Spring Boot, and Kubernetes";
         String actualTitle = "";
-        //        Given user has logged into Medium application
-//        When user searches for ‘spring boot’
-//        Then user should see ‘How to scale Microservices with Message Queues, Spring Boot, and Kubernetes’ as the first article
+
         if (isCurrentPlatformWeb(platform)) {
             webHomePage = new WebHomePage(platformDriver);
-            webLoginPage = new WebLoginPage(platformDriver);
 
             webHomePage.openPage();
-            webHomePage.goToTwitterLoginPage();
-
-            webLoginPage.fillTwitterLoginCredentials(username, password);
 
             webHomePage.searchForArticle(searchTitle);
             actualTitle = webHomePage.getFirstArticleTitle();
@@ -90,16 +84,37 @@ public class TestRunner {
 
         if (isCurrentPlatformMobile(platform)) {
             mobileHomePage = new MobileHomePage((AppiumDriver) platformDriver);
-            mobileLoginPage = new MobileLoginPage((AppiumDriver) platformDriver);
-
-            mobileHomePage.goToTwitterLoginPage();
-
-            mobileLoginPage.fillTwitterLoginCredentials(username, password);
-            mobileLoginPage.waitForHomeTitle();
 
             mobileHomePage.searchForArticle(searchTitle);
             actualTitle = mobileHomePage.getFirstArticleTitle();
         }
         assertThat(actualTitle, equalTo(expectedTitle));
     }
+
+    @Test(dataProvider = "drivers")
+    public void ut2(String platform, RemoteWebDriver platformDriver) {
+        if (isCurrentPlatformWeb(platform)) {
+            webHomePage = new WebHomePage(platformDriver);
+            webMembershipPage = new WebMembershipPage(platformDriver);
+
+            webHomePage.openPage();
+
+            webHomePage.goToMembershipPage();
+
+            assertThat("Price not match",webMembershipPage.getMonthlySubsPrice() == 5.00);
+            assertThat("Price not match", webMembershipPage.getAnnualSubsPrice() == 50.00);
+        }
+
+        if (isCurrentPlatformMobile(platform)) {
+            mobileHomePage = new MobileHomePage((AppiumDriver) platformDriver);
+            mobileMembershipPage = new MobileMembershipPage((AppiumDriver) platformDriver);
+
+            mobileHomePage.goToSettingsPage();
+            mobileHomePage.goToMembershipPage();
+
+            assertThat("Price not match", mobileMembershipPage.getMonthlySubsPrice() == 4.99);
+            assertThat("Price not match", mobileMembershipPage.getAnnualSubsPrice() == 49.99);
+        }
+    }
+
 }
